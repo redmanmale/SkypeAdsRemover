1d647e8303a9e5b5bf358d4d71b5e1d5f9d6173f
redmanmale <redmanmale@gmail.com>
2016-11-22 22:01:03 +0300

    Fixed run condition; updated solution file

--- SkypeAdsRemover/Program.cs
+++ SkypeAdsRemover/Program.cs
@@ -12,12 +12,11 @@ namespace Redmanmale.SkypeAdsRemover
         private const string adPattern = "<AdvertPlaceholder>1</AdvertPlaceholder>";
         private const string adDisabledPattern = "<AdvertPlaceholder>0</AdvertPlaceholder>";
 
-        private static readonly Regex _finder = new Regex(adPattern,
-            RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+        private static readonly Regex _finder = new Regex(adPattern, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
 
         private static void Main(string[] args)
         {
-            if (args == null || args.Length != 0 && !string.IsNullOrWhiteSpace(args[0]))
+            if (args == null || args.Length == 0 || string.IsNullOrWhiteSpace(args[0]))
             {
                 Console.WriteLine("Provide skype profile in argument");
                 return;
